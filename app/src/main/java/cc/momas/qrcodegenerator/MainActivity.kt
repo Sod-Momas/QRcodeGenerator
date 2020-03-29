3eb4df4a5ec1f791369413ba3dd66fa0f0ada34d
Sod-Momas <sothereer@gmail.com>
2020-03-29 14:50:24 +0800

    add generate qr code

--- app/src/main/java/cc/momas/qrcodegenerator/MainActivity.kt
+++ app/src/main/java/cc/momas/qrcodegenerator/MainActivity.kt
@@ -1,12 +1,26 @@
 package cc.momas.qrcodegenerator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val editText = findViewById<EditText>(R.id.editText)
+        val qrcode = findViewById<ImageView>(R.id.qrcode)
+
+        button.setOnClickListener {
+            Toast.makeText(this@MainActivity, "click me!", Toast.LENGTH_SHORT).show()
+            val bitmap = QRCodeUtil.createQRCodeBitmap(editText.text.toString(), 480, 480)
+            qrcode.setImageBitmap(bitmap)
+        }
     }
 }
